eaae9a4ac474f57731eb033ff7ef277c91535fce
AbdulGhani99 <72146661+AbdulGhani99@users.noreply.github.com>
2020-11-07 07:30:37 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-$(document).click(function(){
+$(document).ready(function(){
 
 $('.next').click(function(){
+
+    
     var currentImg = $('.active');
     var nextImg = currentImg.next();
 
-
     var len = nextImg.length;
+    
+   
+    alert(len);
+
     if(len > 0){
+
+
         currentImg.removeClass('active');
         nextImg.addClass('active');
-    }
+        }
+    // else if(len==0){
+    //     $('img:nth-child(0)').addClass=('active');
+    //     }
+    
  });
 
 
